7809c083240b96b7af46818f227e9eca260c9e54
Aloy <aloyleow91@gmail.com>
2025-02-23 16:47:42 +0800

    Feat: Setuped uploadShoes

--- controllers/uploadShoes.ts
+++ controllers/uploadShoes.ts
@@ -1,4 +1,4 @@
-import express, {type Request, type Response} from "express";
+import express, { type Request, type Response } from "express";
 import { z } from "zod";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import saveImage from "../services/multerStorage";
@@ -20,7 +20,7 @@ const uploadShoesSchema = z.object({
 
 type UploadShoes = z.infer<typeof uploadShoesSchema>;
 
-router.post("/upload", saveImage.single('imageAWS'), async (req: Request<{},{}, UploadShoes>, res: Response) => {
+router.post("/upload", saveImage.single('imageAWS'), async (req: Request<{}, {}, UploadShoes>, res: Response) => {
 
   const bucketName = process.env.BUCKET_NAME;
 
@@ -44,12 +44,6 @@ router.post("/upload", saveImage.single('imageAWS'), async (req: Request<{},{}, 
   SELECT sizeid FROM sizes
   WHERE sizeid = $1
   `
-  // const awsParams = {
-  //   Bucket: bucketName,
-  //   Key: req.file?.originalname,
-  //   Body: req.file?.buffer,
-  //   ContentType: req.file?.mimetype
-  // }
 
   try {
 
@@ -57,11 +51,22 @@ router.post("/upload", saveImage.single('imageAWS'), async (req: Request<{},{}, 
     if (!validateReqBody.success) {
       const validateError = validateReqBody.error.issues.map(item => `${item.path}: ${item.message}`);
       throw new Error(`Validation type failed ${validateError}`);
-    } 
+    }
 
     const checkType = await pool.query(queryType, [req.body.typeid]);
+    if (checkType.rowCount === 0 || !checkType) {
+      throw new Error("Shoe Type does not exist.");
+    }
+
     const checkBrand = await pool.query(queryBrand, [req.body.brandid]);
+    if (checkBrand.rowCount === 0 || !checkBrand) {
+      throw new Error("Shoe Brand does not exist.");
+    }
+
     const checkSize = await pool.query(querySize, [req.body.sizeid]);
+    if (checkSize.rowCount === 0 || !checkSize) {
+      throw new Error("Shoe Size does not exist.");
+    }
 
     const dataInput = [
       req.body.name,
@@ -72,34 +77,48 @@ router.post("/upload", saveImage.single('imageAWS'), async (req: Request<{},{}, 
       req.body.miscellaneous,
       req.body.costprice,
       req.body.picture
-    ] 
+    ]
+
+    const upload = await pool.query(dataUpload, dataInput);
+    if (!upload) {
+      throw new Error("PG Database Error.");
+    }
+
+    if (req.body.picture) {
+
+      const awsParams = {
+        Bucket: bucketName,
+        Key: upload.rows[0].brandid,
+        Body: req.file?.buffer,
+        ContentType: req.file?.mimetype
+      }
+      const command = new PutObjectCommand(awsParams)
+
+      const uploadImage = await s3.send(command)
+      if (!uploadImage) {
+        throw new Error("AWS Database Error.");
+      }
+
+      res.status(201).json("Success");
+
+    }
+
+    res.status(201).json("Success w/o Image");
 
 
-  } catch {
-      
+  } catch (error: unknown){
+
+    if (error instanceof Error) {
+
+      res.status(500).json({ error: error.message });
+
+    } else {
+
+      res.status(500).json({ error: "Internal server error" });
+
+    }
+
   }
 })
-
-// router.post("/test", async (req, res) => {
-//   const queryUpload = `
-//   INSERT INTO brands (brandname)
-//   VALUES ($1)
-//   RETURNING brandid
-//   `
-
-//   try {
-//     const upload = await pool.query(queryUpload, [req.body.brandname]);
-// //returning id while created
-//     res.status(201).json(upload.rows[0].brandid);
-   
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       res.status(500).json({ error: error.message})
-//     } 
-//     res.status(500).json({ error: "major error" })
-//   }
-
-
-// })
 
 export default router
